d02b451d293f6c597e80bbfff842479199252d8a
HafsaHafees <coderhafsa@gmail.com>
2025-06-03 18:15:00 +0100

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,12 @@
+def add():
+    a=10
+    b=20
+    print(a+b)
+add()
+def sub(y,x):
+    print(y-x)
+sub(20,10)
+sub(50,35)
+num1= int(input("Enter number"))
+num2= int(input("Enter number"))
+sub(num1,num2)
