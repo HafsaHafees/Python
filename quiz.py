b5189bb2faaa7f40fe030805d3c771932653312e
HafsaHafees <coderhafsa@gmail.com>
2025-06-23 18:25:13 +0100

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,30 @@
+print("What is the most important organ in the human body?")
+print("a. lungs")
+print("b. kidneys")
+print("c. brain")
+print("d. heart")
+i= input("Choose correct option:")
+if i=="d":
+    print("CORRECT")
+    print("What is the most accurate device to check your pulse?")
+    print("a. clock")
+    print("b. stop watch")
+    print("c. phone")
+    print("d. doctor appointment")
+    n= input("Choose correct option:")
+    if n=="d":
+        print("CORRECT")
+        print("Which organ is the most vital for breathing?")
+        print("a. kidney")
+        print("b. lungs")
+        print("c. intestines")
+        print("d. veins")
+        p= input("Choose correct option:")
+        if p=="b":
+            print("WELLDONE YOU HAVE COMPLETED THE QUIZ!!!")
+        else:
+            print("TRY AGAIN")
+    else:
+        print("INNCORRECT TRY AGAIN")
+else:
+    print("INNCORRECT TRY AGAIN")
